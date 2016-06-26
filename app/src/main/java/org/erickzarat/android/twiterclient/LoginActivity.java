03345f6bad63ed87dd29b910097ee2f3079ee138
ErickZarat <ezarat15@gmail.com>
2016-06-26 12:17:02 -0600

    login with twitter

--- app/src/main/java/org/erickzarat/android/twiterclient/LoginActivity.java
+++ app/src/main/java/org/erickzarat/android/twiterclient/LoginActivity.java
@@ -1,23 +1,58 @@
 package org.erickzarat.android.twiterclient;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RelativeLayout;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import com.twitter.sdk.android.Twitter;
-import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.*;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 import io.fabric.sdk.android.Fabric;
+import org.erickzarat.android.twiterclient.main.MainActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
-    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
-    private static final String TWITTER_KEY = "Nt8K62VWhJngfr3pJqflVmUOE";
-    private static final String TWITTER_SECRET = "RyaaeLJUUx3PmOzHOEwkOxd08KABDjmxkfix9xIoAc8YDExfWh";
+    @Bind(R.id.twitterLoginButton)
+    TwitterLoginButton twitterLoginButton;
+    @Bind(R.id.container)
+    RelativeLayout container;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
-        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        if (Twitter.getSessionManager().getActiveSession() != null){
+            navigateToMainScreen();
+        }
+
+        twitterLoginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                navigateToMainScreen();
+            }
+
+            @Override
+            public void failure(TwitterException e) {
+                String msgError = String.format(getString(R.string.login_error_message),
+                        e.getLocalizedMessage());
+                Snackbar.make(container, msgError, Snackbar.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        twitterLoginButton.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void navigateToMainScreen() {
+        startActivity(new Intent(this, MainActivity.class));
     }
 }
---
+++ app/src/main/java/org/erickzarat/android/twiterclient/main/MainActivity.java
@@ -0,0 +1,14 @@
+package org.erickzarat.android.twiterclient.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import org.erickzarat.android.twiterclient.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
